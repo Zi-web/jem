1f9e37b0378ee3a5645055969123948494f101d2
Zi-web <37513694+Zi-web@users.noreply.github.com>
2020-04-02 20:14:13 +0300

    Add files via upload

---
+++ js/popap.js
@@ -0,0 +1,23 @@
+document.getElementById("btn__call1").onclick = function() {showForm()};
+      function showForm() {
+    document.querySelector('.overlay').classList.add("show");
+    document.querySelector('.popap-wrapper').classList.add("show-form");
+}
+
+document.getElementById("closeform").onclick = function() {closeForm()};
+      function closeForm() {
+    document.querySelector('.overlay').classList.remove("show");
+    document.querySelector('.popap-wrapper').classList.remove("show-form");
+}
+
+document.getElementById("btn__call2").onclick = function() {showForm()};
+      function showForm() {
+    document.querySelector('.overlay').classList.add("show");
+    document.querySelector('.popap-wrapper').classList.add("show-form");
+}
+
+document.getElementById("closeform").onclick = function() {closeForm()};
+      function closeForm() {
+    document.querySelector('.overlay').classList.remove("show");
+    document.querySelector('.popap-wrapper').classList.remove("show-form");
+}
